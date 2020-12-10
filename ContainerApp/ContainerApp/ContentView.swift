8d50c44b4571ab8c39eec431af385a3ee576b61b
bibi <65612292+seonandseon@users.noreply.github.com>
2020-12-10 10:53:43 +0900

    first commit

---
+++ ContainerApp/ContainerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContainerApp
+//
+//  Created by bibi lim on 2020/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
